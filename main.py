d8639a1b0b9b889c8ace50f17563fb2b7ed7b404
Dewi-Payne <dewitbpayne@gmail.com>
2021-07-30 20:51:07 +0100

    Implemented Vigenere cipher and improved Caesar cipher

--- main.py
+++ main.py
@@ -7,18 +7,33 @@ def caesar(message: str, key: int):
 
     # TODO - deal with handling capital letters
     final_message = ""
-    for character in message:
-        final_message += chr((ord(character) + key - 97) % 26 + 97)
+    for character in message.lower():
+        if character.isalpha():
+            final_message += chr((ord(character) + key - 97) % 26 + 97)
+        else:
+            final_message += character
     return final_message
 
 
-def vigenere(message: str, key: int):
+def vigenere(message: str, key: str):
     # Vigenère cipher - slightly more advanced cipher that uses caesar ciphers to shift a message by
-    # a different number for each character.
-    # TODO
-    pass
+    # a different number for each character based on the key.
 
+    while len(key) < len(message):
+        key += key
+
+    key = key[:len(message)].lower()
+
+    final_message = ""
+
+    for i, character in enumerate(message.lower()):
+        if character.isalpha():
+            final_message += chr((ord(character) + ord(key[i]) - (97 * 2)) % 26 + 97)
+        else:
+            final_message += character
+    return final_message
 
 if __name__ == "__main__":
-    print(caesar("oguucig", -2))
-    print(vigenere("message", 2))
+    test_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    print(caesar(test_text, -2))
+    print(vigenere(test_text, "king"))
